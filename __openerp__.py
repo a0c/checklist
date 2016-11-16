9d9e785f48960201d68f1ff4aba6e79ce784718a
Anton Chepurov <anton.chepurov@gmail.com>
2016-11-16 12:24:12 +0200

    initial commit

---
+++ __openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Checklists TMP',
+    'version': '0.1',
+    'category': 'Warehouse',
+    'summary': 'Checklists TMP',
+    'description': """
+Checklists TMP
+==============
+""",
+    'depends': [
+    ],
+    'data': [
+    ],
+    'qweb': [
+    ],
+}

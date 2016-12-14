cc7beb2e139b43844673eb798e132d8c99777575
Anton Chepurov <anton.chepurov@gmail.com>
2016-12-14 23:27:52 +0200

    checklist task: managers edit description, users edit notes

--- wizard/task_edit.py
+++ wizard/task_edit.py
@@ -4,11 +4,11 @@ from openerp import api, fields, models
 class task_edit(models.TransientModel):
     _name = 'task.edit'
 
-    description = fields.Text(default=lambda self: self.get_task().description)
+    description = fields.Text(default=lambda self: self.get_task().notes)
 
     def get_task(self):
         return self.env['project.task'].browse(self.env.context['active_ids'])[0]
 
     @api.multi
     def do_save(self):
-        self.get_task().description = self.description
+        self.get_task().notes = self.description
